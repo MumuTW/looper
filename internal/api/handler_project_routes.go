dac179e66a6344afda3bca79f69fa3a0641adfe4
mumuTW <clothl47364@gmail.com>
2026-07-31 04:56:26 +0800

    refactor(api): complete loop create handler

--- internal/api/handler_project_routes.go
+++ internal/api/handler_project_routes.go
@@ -7,10 +7,10 @@ import (
 	"net/http"
 	"strings"
 
-	"github.com/nexu-io/looper/internal/config"
-	"github.com/nexu-io/looper/internal/projects"
-	"github.com/nexu-io/looper/internal/storage"
-	pkgapi "github.com/nexu-io/looper/pkg/api"
+	"github.com/MumuTW/looper/internal/config"
+	"github.com/MumuTW/looper/internal/projects"
+	"github.com/MumuTW/looper/internal/storage"
+	pkgapi "github.com/MumuTW/looper/pkg/api"
 )
 
 func (h *Handler) buildProjectsRouteResponse(r *http.Request) (any, error) {
